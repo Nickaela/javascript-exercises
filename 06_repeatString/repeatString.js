da12975b876c7d6d86f4bf02ebb0e3636da4a417
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-08-29 21:53:09 +0800

    commit these

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    for(let i = 0; i < 3; i++){
+        console.log('hey');
+    }
 };
+
+console.log(repeatString);
+console.log('unggoi');
 
 // Do not edit below this line
 module.exports = repeatString;
